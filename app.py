d17b17930ba99ff28b810f9c9b335940eab3637b
ggxxding <ggxxding@163.com>
2025-03-10 14:53:17 +0800

    test

--- app.py
+++ app.py
@@ -29,7 +29,9 @@ preprocess = transforms.Compose(
 
 def deepfake_image_detection(image):
     image_tensor = toTensors(image)
+    print(image_tensor.shape)
     NPR_ = interpolate(image_tensor, 0.5)
+    print(NPR_.shape)
     NPR = image_tensor - NPR_
     NPR = rz_func3(toPIL(NPR))
     NPR_ = toPIL(NPR_)
@@ -37,8 +39,11 @@ def deepfake_image_detection(image):
     output, flat, NPR1_ = model(image)
     output = output.sigmoid().flatten().tolist()[0]
     flat = rz_func2(toPIL(flat))
+    if output > 0.5:
+        gr.Warning("警告：图像为伪造！", duration = None)
 
-    return [NPR, flat, {"Real": 1-output, "Fake": output }]
+    return [NPR, flat, {"真实": 1-output, "伪造": output }]
+
 
 if __name__ == "__main__":
     model = efficientnet_b1(num_classes = 1)
@@ -50,17 +55,48 @@ if __name__ == "__main__":
     #         '/mnt/share_data/dmj/phase1_converted/train/0_real/f7ade355ac7d53dfdd39b2d71d72bfa2.jpg',
     #         '/mnt/share_data/dmj/phase1_converted/train/1_fake/b1e9f64896e0b11945a9946258f7218d.jpg',
     #         '/mnt/share_data/dmj/phase1_converted/train/1_fake/6ad80d61f821a5e58878d58a2018eb48.jpg']
-    imgs = ['./src/d80534da7b09be2684de429be22b8b9d.jpg',
+    imgs = ['/mnt/share_data/dmj/phase1_converted/val/0_real/fdfd6d28c38420e7872b85d5419c44ae.jpg',
+            '/mnt/share_data/dmj/phase1_converted/val/1_fake/fd448eca0f3a66ea6dac2fcb423c441d.jpg',
+        './src/d80534da7b09be2684de429be22b8b9d.jpg',
             './src/f7ade355ac7d53dfdd39b2d71d72bfa2.jpg',
             './src/b1e9f64896e0b11945a9946258f7218d.jpg',
             './src/6ad80d61f821a5e58878d58a2018eb48.jpg']
-    demo = gr.Interface(
-        fn=deepfake_image_detection,
-        inputs=[gr.Image(type='pil')],
-        outputs=[gr.Image(label = 'Neighboring pixel relationships',type='pil'),
-                 gr.Image(label = 'Lase hidden layer',type='pil'),
-                 gr.Label(num_top_classes=2)],
-        examples = imgs
-    )
 
-    demo.launch()
+    warn_css = """
+    h1{
+    text-align:center;
+    }
+    .toast-wrap {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,300%);
+        z-index: 9999;
+    }
+    """
+
+    with gr.Blocks(css = warn_css) as demo:
+        gr.Markdown("# Deepfake合成人脸图像检测工具V1.0")
+        with gr.Row():
+            with gr.Column():
+                input_img = [gr.Image(label="上传图像")]
+                btn = gr.Button("提交")
+
+                gr.Examples(
+                    examples=['/mnt/share_data/dmj/phase1_converted/val/0_real/fdfd6d28c38420e7872b85d5419c44ae.jpg',
+                    '/mnt/share_data/dmj/phase1_converted/val/1_fake/fd448eca0f3a66ea6dac2fcb423c441d.jpg',
+                './src/d80534da7b09be2684de429be22b8b9d.jpg',
+                    './src/f7ade355ac7d53dfdd39b2d71d72bfa2.jpg',
+                    './src/b1e9f64896e0b11945a9946258f7218d.jpg',
+                    './src/6ad80d61f821a5e58878d58a2018eb48.jpg'],
+                    inputs=input_img
+                )
+            with gr.Column():
+                output = [gr.Image(label = 'NPR层',type='pil'),
+                    gr.Image(label = 'Softmax层',type='pil'),
+                    gr.Label(num_top_classes=2,label = "预测结果")]
+
+            btn.click(deepfake_image_detection, inputs=input_img, outputs=output)
+
+
+    demo.launch(share = True)
---
+++ app_flask.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template, send_file
+import qrcode
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/get_qrcode')
+def home():
+    """生成二维码，指向上传页面"""
+    upload_url = "http://127.0.0.1:5000/upload"  # 服务器部署后需替换为公网 URL
+    qr = qrcode.make(upload_url)
+    qr_path = os.path.join(UPLOAD_FOLDER, 'upload_qr.png')
+    qr.save(qr_path)
+    return send_file(qr_path, mimetype='image/png')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    """文件上传页面"""
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+            file.save(file_path)
+            return "文件上传成功！"
+    
+    return '''
+    <!doctype html>
+    <html>
+    <body>
+        <h2>上传图片</h2>
+        <form method="POST" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <input type="submit" value="上传">
+        </form>
+    </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
